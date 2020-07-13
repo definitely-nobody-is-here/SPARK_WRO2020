fe88f2320b6bb35cbda99dc3f7053ed91b85275c
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-07-12 21:31:53 -0400

    Add robot interface

---
+++ WRO2020/src/main/java/spark/wro/Robot.java
@@ -0,0 +1,43 @@
+package spark.wro;
+
+public class Robot {
+	public void forward(float cm, int speed, int stopLine) {
+		//TODO:forward
+	}
+	public void backward(float cm, int speed, int stopLine) {
+		//TODO:forward
+	}
+	public void turn(int type, int degree, int stopLine) {
+		//TODO:forward
+	}
+	public void followLine(float cm, int speed, int stopLine) {
+		//TODO:forward
+	}
+	public void turnMotor(int motor, float degree, int angularSpeed) {
+		//TODO:forward
+	}
+	public void readReflect(int port) {
+		//TODO:forward
+	}
+	public void readColor(int port) {
+		//TODO:forward
+	}
+	public void init() {
+		//TODO:??????
+	}
+	public void getEV3() {
+		//TODO:forward
+	}
+	public void arc(float cm, int speed, int stopLine) {
+		//TODO:forward
+	}
+	public void reset() {
+		//TODO:forward
+	}
+	public void align() {
+		//TODO:forward
+	}
+	public void square() {
+		//TODO:forward
+	}
+}
