12fbee79541f172e0dbeaa14d62151fd1ae3a8d5
Radioactive64 <jackie.z.gao@gmail.com>
2020-07-12 21:36:01 -0400

    Updated TODO
    Names

--- WRO2020/src/main/java/spark/wro/Robot.java
+++ WRO2020/src/main/java/spark/wro/Robot.java
@@ -2,42 +2,42 @@ package spark.wro;
 
 public class Robot {
 	public void forward(float cm, int speed, int stopLine) {
-		//TODO:forward
+		//TODO:J
 	}
 	public void backward(float cm, int speed, int stopLine) {
-		//TODO:forward
+		//TODO:J
 	}
 	public void turn(int type, int degree, int stopLine) {
-		//TODO:forward
+		//TODO:J
 	}
 	public void followLine(float cm, int speed, int stopLine) {
-		//TODO:forward
+		//TODO:M
 	}
 	public void turnMotor(int motor, float degree, int angularSpeed) {
-		//TODO:forward
+		//TODO:M
 	}
 	public void readReflect(int port) {
-		//TODO:forward
+		//TODO:J
 	}
 	public void readColor(int port) {
-		//TODO:forward
+		//TODO:J
 	}
 	public void init() {
-		//TODO:??????
+		//TODO:J
 	}
 	public void getEV3() {
-		//TODO:forward
+		//TODO:M
 	}
 	public void arc(float cm, int speed, int stopLine) {
-		//TODO:forward
+		//TODO:M
 	}
 	public void reset() {
 		//TODO:forward
 	}
 	public void align() {
-		//TODO:forward
+		//TODO:M
 	}
 	public void square() {
-		//TODO:forward
+		//TODO:M
 	}
 }
