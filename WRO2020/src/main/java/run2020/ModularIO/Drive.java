15d57fdf05f4b6fe43214a203864a41108b899da
Radioactive64 <jackie.z.gao@gmail.com>
2020-07-17 20:40:08 -0400

    no

--- WRO2020/src/main/java/run2020/ModularIO/Drive.java
+++ WRO2020/src/main/java/run2020/ModularIO/Drive.java
@@ -2,6 +2,10 @@ package run2020.ModularIO;
 
 public final class Drive extends ModularIO {
 	
+	public Drive(double wheelDiameter, double wheelDistance, boolean reversed) {
+		super(wheelDiameter, wheelDistance, reversed);
+	}
+
 	private void CheckPowerLimits(int power) {
 		
 		if (Math.abs(power) > 700) {
